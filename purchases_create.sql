6a39da368b13540ea36a8d7ce52eb80c8120bc9c
Emily Boyle <emilycboyle95@gmail.com>
2021-01-12 16:03:38 +0100

    creating purchases table

---
+++ purchases_create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE closet.purchases (
+    purchase_id      serial PRIMARY KEY,
+    garment_id       serial,
+    purchase_date    date,
+    purchased_from   varchar(255) not NULL,
+    price            integer,
+    thrifted         boolean
+);
+-- create table purchases for purchase information
