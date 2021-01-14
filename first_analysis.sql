84ceb6eda2bba02d9155d2d22da75b98b7c098c7
Emily Boyle <emilycboyle95@gmail.com>
2021-01-14 16:22:45 +0100

    analysis on brand vs. purchase location

--- first_analysis.sql
+++ first_analysis.sql
@@ -8,3 +8,8 @@ SELECT COUNT(garment_id) AS garment_no,
 GROUP BY brand;
 -- work out the count for each brand of garments 
 
+SELECT COUNT(purchase_id) AS garment_no,
+    purchased_from
+FROM closet.purchases
+GROUP BY purchased_from;
+-- count by purchase location, compare with brand query 
